e5aa3ba242d6ce746f4f3a4f7d439425f753748b
pornpol <pornpol.w@gmail.com>
2020-09-13 20:12:22 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const [time, setTime] = useState(new Date());
+
+    return (
+        <div className='App'>
+            <h1 className='App-header'>{time.toString()}</h1>
+        </div>
+    );
 }
 
 export default App;
